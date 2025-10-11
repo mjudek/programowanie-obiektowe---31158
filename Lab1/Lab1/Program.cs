6f18ef3f66667bfb9b1de1a8196d447a712b101c
Mikołaj Judek <mjudek@edu.cdv.pl>
2025-10-11 13:17:18 +0200

    project1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Project1
 
-Console.WriteLine("Hello, World!");
+const int RequiredAge = 14;
+const int SimRequiredAge = 18;
+const string AccessDeniedMessage = "Musisz miec 14 lat";
+const string Wiad = "Zapraszamy!";
+const string SimDeniedMessage = "Jestes za mlody zeby kupic karte SIM";
+
+int age = 19;
+
+if (age >= SimRequiredAge)
+{
+    Console.WriteLine(Wiad);
+}
+else if (age < RequiredAge)
+{
+    Console.WriteLine(AccessDeniedMessage);
+}
+else
+{
+    Console.WriteLine(SimDeniedMessage);
+}
