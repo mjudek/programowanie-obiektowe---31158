faa862335d6cdfb0d090f8b5526447d158fdcf1f
Mikołaj Judek <mjudek@edu.cdv.pl>
2025-10-11 14:10:24 +0200

    project1

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,12 +1,18 @@
 ﻿// Project1
 
+/*
+//Zadanie "domowe"
 const int RequiredAge = 14;
 const int SimRequiredAge = 18;
 const string AccessDeniedMessage = "Musisz miec 14 lat";
 const string Wiad = "Zapraszamy!";
 const string SimDeniedMessage = "Jestes za mlody zeby kupic karte SIM";
 
-int age = 19;
+int age;
+
+Console.Write("Podaj wiek: ");
+age = Convert.ToInt32(Console.ReadLine());
+
 
 if (age >= SimRequiredAge)
 {
@@ -20,3 +26,33 @@ else
 {
     Console.WriteLine(SimDeniedMessage);
 }
+
+//Petla While
+int count = 0;
+
+while (count < 5)
+{
+    Console.WriteLine("Numer Iteracji  " + count);
+    count++;
+}
+
+//petla Do While
+string password;
+do
+{
+    Console.Write("Podaj haslo: ");
+    password = Console.ReadLine();
+
+} while (password != "admin123");
+
+Console.WriteLine("Zalogowano pomyslnie!");
+*/
+
+int number;
+do
+{
+    Console.Write("Podaj liczbę wiekszą od 0:  ");
+    number = Convert.ToInt32(Console.ReadLine());
+} while (number < 0 && number != 0);
+Console.WriteLine("Gratulacje");
+
